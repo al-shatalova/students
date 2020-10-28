eb5e53869877cd8571de94cf9e212577cdc5606d
Yan Novak (yann) <yann@ozon.ru>
2020-10-28 17:03:03 +0300

    [Novak]: Переписал доп задание в lab5 на классы

--- Novak_PI17-1/lab5/dop.js
+++ Novak_PI17-1/lab5/dop.js
@@ -1,14 +1,23 @@
-const itemsCount = [0, 0, 0]
-const dict = {
-    0: 'Красное',
-    1: 'Черное',
-    2: 'Белое',
+class Game {
+    static pluralize = num => ([1, 5, 6, 7, 8, 9, 0].includes(num)) ? 'раз' : 'раза'
+
+    static dict = {
+        0: 'Красное',
+        1: 'Черное',
+        2: 'Белое',
+    }
+
+    start() {
+        const itemsCount = [0, 0, 0]
+
+        for (let i = 0; i < 1000000; i++) {
+            itemsCount[Math.floor(Math.random() * itemsCount.length)]++
+        }
+
+        const maxCountIndex = itemsCount.indexOf(Math.max(...itemsCount))
+        alert(`${Game.dict[maxCountIndex]} значение (${maxCountIndex}) выпало ${itemsCount[maxCountIndex]} ${Game.pluralize(itemsCount[maxCountIndex])} `)
+    }
 }
 
-for (let i = 0; i < 1000000; i++) {
-    itemsCount[Math.floor(Math.random() * itemsCount.length)]++
-}
-
-const pluralize = num => ([1, 5, 6, 7, 8, 9, 0].includes(num)) ? 'раз' : 'раза'
-const maxCountIndex = itemsCount.indexOf(Math.max(...itemsCount))
-alert(`${dict[maxCountIndex]} значение (${maxCountIndex}) выпало ${itemsCount[maxCountIndex]} ${pluralize(itemsCount[maxCountIndex])} `)
+const g = new Game()
+g.start()
