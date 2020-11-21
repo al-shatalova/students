98707e653b93fee96ed07fb2588a990beb466211
Vladimir <girba.vladimir@yandex.ru>
2020-11-21 13:43:29 +0300

    node js

---
+++ laba-npm/index.jsx
@@ -0,0 +1,67 @@
+class TodoApp extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = { items: [], text: '' };
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+
+    render() {
+        return (
+            <div>
+                <h3>TODO</h3>
+                <form onSubmit={this.handleSubmit}>
+                    <label htmlFor="new-todo">
+                        What needs to be done?
+                    </label>
+                    <input
+                        id="new-todo"
+                        onChange={this.handleChange}
+                        value={this.state.text}
+                        placeholder={"Write your task"}
+                    />
+                    <button>
+                        Add #{this.state.items.length + 1}
+                    </button>
+                </form>
+                <TodoList items={this.state.items} />
+            </div>
+        );
+    }
+
+    handleChange(e) {
+        this.setState({ text: e.target.value });
+    }
+
+    handleSubmit(e) {
+        e.preventDefault();
+        if (this.state.text.length === 0) {
+            return;
+        }
+        const newItem = {
+            text: this.state.text,
+            id: Date.now()
+        };
+        this.setState(state => ({
+            items: state.items.concat(newItem),
+            text: ''
+        }));
+    }
+}
+
+class TodoList extends React.Component {
+    render() {
+        return (
+            <ul>
+                {this.props.items.map(item => (
+                    <li key={item.id}>{item.text}</li>
+                ))}
+            </ul>
+        );
+    }
+}
+
+ReactDOM.render(
+    <TodoApp />,
+    document.getElementById('todos-example')
+);
